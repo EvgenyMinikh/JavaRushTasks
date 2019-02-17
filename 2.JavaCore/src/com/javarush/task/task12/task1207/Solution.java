a6bb6a9fcc056b4ae5715b00d68fefd1e969aaa0
Evgeny Minih <evgeny.minih@nokiantyres.com>
2019-02-17 17:17:10 +0300

    [Add] Solved tasks 17.02.2019

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(3);
+        print(new Integer(3));
+    }
+
+    public static void print(int a) {}
+
+    public static void print(Integer a) {}
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public interface CanFly {
+        void m1();
+    }
+
+    public interface CanRun {
+        void m2();
+    }
+
+    public interface CanSwim {
+        void m3();
+    }
+//add interfaces here - добавь интерфейсы тут
+
+}
